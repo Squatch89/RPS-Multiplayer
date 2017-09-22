73e871d61aa190ca3ee7da8df7cc5728159eb261
Gene Williams <GeneWillb@Gmail.com>
2017-09-22 11:00:19 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCBUmCDAvOqKxMe0Rbpu-5UZTrin1SGciU",
+    authDomain: "multiplayer-rps-29165.firebaseapp.com",
+    databaseURL: "https://multiplayer-rps-29165.firebaseio.com",
+    projectId: "multiplayer-rps-29165",
+    storageBucket: "",
+    messagingSenderId: "667273371838"
+};
+firebase.initializeApp(config);
